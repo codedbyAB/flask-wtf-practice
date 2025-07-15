e715e14c892b6fda59a376cbeeaac2f08be0fedf
codedbyAB <abner5353@gmail.com>
2025-07-15 09:24:18 -0400

    Updated layout and login HTML

--- forms.py
+++ forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 
-class UserForm(FlaskForm): #created one form class
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
-    ss = StringField("Social Security", validators=[DataRequired()])
 
+class UserRegistration(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField ('Password', validators=[DataRequired(), Length(min=5)])
+    confirm_password = PasswordField("Confirm_Password", validators=[DataRequired(),
+    Length(min=5), EqualTo('password', message='Password must match')])
+    submit = SubmitField('Register')
 
 
--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 from flask import Flask, render_template
-from forms import UserForm
+from forms import UserRegistration
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345'
 
+@app.route('/')
+@app.route('/login',methods=['GET', 'POST'])
+def login_page():
+    form = UserRegistration()
 
-@app.route("/", methods=['GET','POST'])
-@app.route("/login", methods=['GET','POST'])
-def login():
-    form = UserForm()
+
     return render_template('login.html', form=form)
-
-
-
-@app.route('/movies')
-def show_movies():
-    movies = [
-        {"title": "Jurassic Park", "year": 1993, "director": "Steven Spielberg"},
-        {"title": "The Lost World: Jurassic Park", "year": 1997,
-         "director": "Steven Spielberg"}
-    ]
-    return render_template('movies.html', movies=movies)
-
-
-
-
